f7645bb3b87557c5b5d02b9e73e22300d72a77b4
reverie <ss.saswatsahoo@gmail.com>
2023-10-24 02:55:31 +0530

    Added one more instrument

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 
 
-
+from datetime import datetime
 from src.scrapper.script import CandleScrapper
 from src.utilities.enums import InstrumentKey
 
 
-CandleScrapper(instrument_key=InstrumentKey.NIFTY_50.value).fetch_missing_historical_data()
+CandleScrapper(instrument_key=InstrumentKey.NIFTY23OCT19550PE.value).fetch_historical_data(
+    start_date=datetime(
+        year=2023,
+        month=10,
+        day=1
+    )
+)
