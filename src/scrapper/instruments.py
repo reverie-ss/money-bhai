cafba017b11041dda2f07d309e823bfab06aa56b
reverie <ss.saswatsahoo@gmail.com>
2023-11-04 20:37:09 +0530

    Added script to scrap instruments and stoer in database

--- src/scrapper/instruments.py
+++ src/scrapper/instruments.py
@@ -1,11 +1,13 @@
+import pandas as pd
 import pymongo
+import gzip
 import os
 import requests
-import time
+from typing import Dict
 from dateutil import parser
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
-from src.models.data_model_candle import Candle
+from src.models.data_model_candle import Candle, Instruments
 from src.utilities.enums import InstrumentKey
 
 load_dotenv()
@@ -17,6 +19,8 @@ class InstrumentsScrapper:
         client = pymongo.MongoClient(os.environ.get("MONGO_URL"))
         database = client.get_database(os.environ.get("DATABASE"))
         self.candles_collection = database.get_collection("MinuteCandles")
+        self.instruments_collection = database.get_collection("instruments")
+        self.local_csv_file_path = "instruments_file.csv"
 
         self.url = "https://assets.upstox.com/market-quote/instruments/exchange/NSE.csv.gz"
 
@@ -28,19 +32,65 @@ class InstrumentsScrapper:
 
             response = requests.get(self.url, timeout=60)
 
-            # Check if the request was successful (status code 200)
             if response.status_code == 200:
-                # Specify the local file path where you want to save the downloaded file
-                local_file_path = "downloaded_file.pdf"
 
-                # Open the local file in binary write mode and write the content of the downloaded file
-                with open(local_file_path, "wb") as file:
-                    file.write(response.content)
+                # Decompress the GZ file and save the decompressed content to a local file
+                with open(self.local_csv_file_path, 'wb') as local_csv_file:
+                    decompressed_content = gzip.decompress(response.content)
+                    local_csv_file.write(decompressed_content)
 
-                print(f"File downloaded and saved as {local_file_path}")
+
+                return pd.read_csv(self.local_csv_file_path)
             else:
-                print(f"Failed to download the file. Status code: {response.status_code}")
+                print(f"Failed to download the PDF. Status code: {response.status_code}")
+
         except Exception as exc:
             print(exc)
 
+        return None
 
+    def filter_instruments(self, instruments_df: pd.DataFrame) -> list[Instruments]:
+        """
+        The instruments file has a lot of data, we just need to filter out the premiums of nifty and  banknifty
+        """
+
+        valid_instrument_type = ["INDEX", "OPTIDX"]
+        valid_exchange = ["NSE_INDEX", "NSE_FO"]
+        valid_index = ["NSE_INDEX|Nifty 50", "NSE_INDEX|Nifty Bank"]
+
+        valid_instruments_list: list[Instruments] = []
+        for index, row in instruments_df.iterrows():
+            instrument: Instruments = Instruments(**row)
+            if instrument.instrument_type in valid_instrument_type and \
+                instrument.exchange in valid_exchange and \
+                (instrument.tradingsymbol.startswith("NIFTY") or instrument.tradingsymbol.startswith("BANKNIFTY")):
+                valid_instruments_list.append(instrument)
+
+            if instrument.instrument_key in valid_index:
+                valid_instruments_list.append(instrument)
+
+        return valid_instruments_list
+
+    def update_instruments_in_db(self, latest_instruments_list: list[Instruments]):
+        """
+        Update/Insert the instruments into the database 
+        """
+        self.instruments_collection.insert_many(latest_instruments_list, ordered=False)
+
+    def scrap_and_store_instruments(self):
+        """
+        Scraps the data from upstox, filters and stores it in the database
+        """
+        instruments_df = self.fetch_instruments()
+
+        if instruments_df:
+            valid_instruments_list: list[Instruments] = self.filter_instruments(instruments_df=instruments_df)
+
+            # Update/Insert the instruments into the database 
+            db_res = self.instruments_collection.insert_many(valid_instruments_list, ordered=False)
+            print("Successfuly completed scraping and storing instruments")
+            print(f"Fetched instruments: {len(valid_instruments_list)}")
+            print(f"Inserted instruments: {len(db_res.inserted_ids)}")
+            print(f"Ignored instruments: {len(valid_instruments_list) - len(db_res.inserted_ids)}")
+        else:
+            print("Failed to fetch instruments from upstox")
