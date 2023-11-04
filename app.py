df0dd99887313deb2f01d0f865c9821d7b3b82af
reverie <ss.saswatsahoo@gmail.com>
2023-11-04 21:45:40 +0530

    Removed execution from app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
+# from src.scrapper.instruments import InstrumentsScrapper
+# CandleScrapper(instrument_key=InstrumentKey.NIFTY23OCT19550PE.value).fetch_historical_data(
+#     start_date=datetime(
+#         year=2023,
+#         month=10,
+#         day=1
+#     )
+# )
 
-
-from datetime import datetime
-from src.scrapper.script import CandleScrapper
-from src.utilities.enums import InstrumentKey
-
-
-CandleScrapper(instrument_key=InstrumentKey.NIFTY23OCT19550PE.value).fetch_historical_data(
-    start_date=datetime(
-        year=2023,
-        month=10,
-        day=1
-    )
-)
+# InstrumentsScrapper().scrap_and_store_instruments()
