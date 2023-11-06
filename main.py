6bf922e3bf6e5ca5a941470171554af80745bae3
reverie <ss.saswatsahoo@gmail.com>
2023-11-07 01:32:02 +0530

    Exposed API to scrap candles

--- main.py
+++ main.py
@@ -2,25 +2,12 @@
 Main module. This is where it all starts.
 TODO: Move the routes to it's respective modules
 """
-# from datetime import datetime
+from datetime import datetime
+from fastapi import FastAPI
 from src.scrapper.candles import CandleScrapper
 from src.order.exit import ExitService
 from src.scrapper.tradebook import TradebookScrapper
 from src.scrapper.instruments import InstrumentsScrapper
-
-# CandleScrapper(instrument_key="NSE_FO|40742").fetch_historical_data(
-#     start_date=datetime(
-#         year=2023,
-#         month=10,
-#         day=1
-#     )
-# )
-
-
-
-from typing import Union
-
-from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -40,6 +27,21 @@ def scrap_tradebook():
     Route used to scrap all the instruments and store in database
     """
     response = TradebookScrapper().fill_tradebook_from_zerodha()
+    return response
+
+
+@app.get("/scrapper/candle/{instrument_key}")
+def scrap_candles(instrument_key: str):
+    """
+    Route used to scrap all the instruments and store in database
+    """
+    response = CandleScrapper(instrument_key=instrument_key).fetch_historical_data(
+        start_date=datetime(
+            year=2023,
+            month=10,
+            day=1
+        )
+    )
     return response
 
 @app.get("/trade/exit/{instrument_key}")
