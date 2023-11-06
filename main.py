8a47c9ebbab9e9e5feafa28b44ed3dd9034b83aa
reverie <ss.saswatsahoo@gmail.com>
2023-11-07 01:16:36 +0530

    Added FAST API to the project

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.scrapper.instruments import InstrumentsScrapper
 # from datetime import datetime
 from src.scrapper.candles import CandleScrapper
 from src.order.exit import ExitService
+from src.scrapper.premium import PremiumScrapper
 
 
 # CandleScrapper(instrument_key="NSE_FO|40742").fetch_historical_data(
@@ -16,4 +17,20 @@ from src.order.exit import ExitService
 # from src.scrapper.premium import PremiumScrapper
 
 
-# PremiumScrapper().execute()
+# PremiumScrapper().fill_tradebook_from_zerodha()
+
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
