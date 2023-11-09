c0dfeef2973dd4b430eecc7cacc2ee44b99d2a2d
reverie <ss.saswatsahoo@gmail.com>
2023-11-10 01:22:04 +0530

    Added common execute API funciton

--- src/utilities/script.py
+++ src/utilities/script.py
@@ -1,8 +1,12 @@
 """
 Scripts module that contains functions that can be used across all modules
 """
+import json
 import os
-from pydantic import BaseModel # pylint: disable=no-name-in-module
+from pydantic import BaseModel
+import requests
+
+from src.utilities.enums import HTTP_Method, UpstoxEndpoint # pylint: disable=no-name-in-module
 
 
 def convert_model_to_dict(list_data_raw: list[BaseModel]):
@@ -16,23 +20,28 @@ def convert_model_to_dict(list_data_raw: list[BaseModel]):
 
     return list_data_formatted
 
-def generate_header(is_authorization_required: bool = False):
-    """
-    Function is used to get the header for the upstox APIs
-    """
-    header = {
-        "Api-Version": "2.0",
-    }
-
-    if is_authorization_required:
-        header["Authorization"] = "Bearer " + os.environ.get("ACCESS_TOKEN")
-    
-    return header
-
 def base_url():
     """
     Returns the base url of upstox
     """
     return os.environ.get("UPSTOX_BASE_URL")
 
-# def execute_api(method: str = "GET", ):
+def execute_api(method: HTTP_Method, endpoint: UpstoxEndpoint, is_authorization_required: bool = False, body: dict = None, headers: dict = None):
+    """
+    Function used to execute APIs. Helps forming the headers and authorization
+    """
+    if not headers:
+        headers = {}
+        
+    headers["Api-Version"] = "2.0"
+
+    if is_authorization_required:
+        headers["Authorization"] = "Bearer " + os.environ.get("ACCESS_TOKEN")
+
+    api_url =  os.environ.get("UPSTOX_BASE_URL") + endpoint.value
+
+    if method == HTTP_Method.GET:
+        return requests.get(api_url, headers=headers, timeout=60)
+    if method == HTTP_Method.POST:
+        return requests.post(api_url, headers=headers, timeout=60, data=json.loads(body))
+    return None
