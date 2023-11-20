8f01884cc0ae6599fbb760fb51e5b9234d209d0f
reverie <ss.saswatsahoo@gmail.com>
2023-11-20 23:13:17 +0530

    Added module comment for entry model

--- src/models/trades.py
+++ src/models/trades.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel
+"""
+Data Model for entry requirements
+"""
+from pydantic import BaseModel # pylint: disable=no-name-in-module
 
 class TradeEntry(BaseModel):
     option_type: str
